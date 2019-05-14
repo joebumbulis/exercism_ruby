da888519ad26b8f2016f8ba776111871ec1cf3fc
joebumbulis <joebumbulis@gmail.com>
2019-05-14 11:09:26 -0500

    updates hamming with zip and count

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,26 +1,13 @@
 class Hamming
+  ARG_ERROR = 'Arguments must be the same length'
+
   def self.compute(x_strand, y_strand)
-    check_ham(x_strand, y_strand) if same_length?(x_strand, y_strand)
+    raise ArgumentError, ARG_ERROR if x_strand != y_strand
+    check_ham(x_strand, y_strand)
   end
 
   private
-  def self.check_ham(x, y)
-    counter = 0
-
-    x.chars.map.with_index do |x_letter, x_index|
-      y.chars.map.with_index do |y_letter, y_index|
-        counter += 1  if x_letter != y_letter && x_index == y_index
-      end
-    end
-    
-    counter
-  end
-
-  def self.same_length?(x, y)
-    if x.length == y.length
-      true
-    else
-      raise ArgumentError, 'Arguments must be the same length.'
-    end
+  def check_ham(x, y)
+    x.chars.zip(y.chars).count{|a, b| a != b}
   end
 end
